56e1276feb6a634a569e1e84cc024ea0f91d4b45
EverGen1 <nextgenerationever@gmail.com>
2015-11-29 13:28:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function pageChangeA{
  var div1 = document.getElementById("1");
  var div2 = document.getElementById("2");
- div1.style.visibility = "visible";
- div2.style.visibility = "hidden";
  document.getElementById("t").innerHTML = document.getElementById("t").innerHTML;
 }
 function pageChaneB{
  var div1 = document.getElementById("1");
  var div2 = document.getElementById("2");
- div1.style.visibility = "hidden";
- div2.style.visibility = "visible";
  document.getElementById("t").innerHTML = document.getElementById("rt1").innerHTML;
+  if(div2.style.visibility == "visible"){
+   div2.style.visibility = "hidden";
+  }else{
+   div2.style.visibility = "visible";
+  }
 }
