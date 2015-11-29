faa688d192e4d797487ae32f4e8e7cd8e08ab57a
EverGen1 <nextgenerationever@gmail.com>
2015-11-29 13:29:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function pageChangeA{
 function pageChaneB{
  var div1 = document.getElementById("1");
  var div2 = document.getElementById("2");
- document.getElementById("t").innerHTML = document.getElementById("rt1").innerHTML;
+ document.getElementById("rt1").innerHTML = document.getElementById("t").innerHTML;
   if(div2.style.visibility == "visible"){
    div2.style.visibility = "hidden";
   }else{
