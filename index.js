8bd00a8073c7cf41947451b7d4a8c47b680c466c
EverGen1 <nextgenerationever@gmail.com>
2015-11-29 13:09:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function pageChangeA{
+ var 1 = document.getElementById("1");
+ var 2 = document.getElementById("2");
+ 1.style.visibility = 'visible';
+ 2.style.visibility = 'hidden';
  document.getElementById("t").innerHTML = document.getElementById("t").innerHTML;
 }
-function pageChangeB{
- var 1
+function pageChaneB{
+ var 1 = document.getElementById("1");
+ var  = document.getElementById("2");
+ 1.style.visibility = 'hidden';
+ 2.style.visibility = 'visible';
+ document.getElementById("rt1").innerHTML = document.getElementById("t").innerHTML;
+}
