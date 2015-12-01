d694c870c6af0afaa21f81ad82d68494068eccd0
EverGen1 <nextgenerationever@gmail.com>
2015-11-30 17:53:56 -0700

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,20 +3,20 @@ var a = document.getElementById("p1");
 var b = document.getElementById("p2");
 var c = document.getElementById("p3");
 function page1(){
- if(a.style.visibility == 'visible'){
-  a.style.visibility = 'hidden';
+ if(a.style.display == 'block'){
+  a.style.visibility = 'none';
  }else{
-  a.style.visibility = 'visible';
+  a.style.visibility = 'block';
  }
- b.style.visibility = 'hidden';
- c.style.visibility = 'hidden';
- a.innerHTML = a.innerHTML;
-function page2(){
- if(b.style.visibility == 'visible'){
-  b.style.visibility = 'hidden';
+ b.style.visibility = 'none';
+ c.style.visibility = 'none';
+ a.innerHTML = a.innerHTML
+function page2()
+ if(b.style.display == 'block'){
+  b.style.visibility = 'none';
  }else{
-  b.style.visibility = 'visible';
+  b.style.visibility = 'block';
  }
-  c.style.visibility = 'hidden';
+  c.style.visibility = 'none';
  c.innerHTML = a.innerHTML;
 }
