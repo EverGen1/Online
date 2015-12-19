c8390385999239c4bfff85df391d0d7492d818ed
EverGen1 <nextgenerationever@gmail.com>
2015-12-19 12:06:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var bodFill = document.getElementById("emptyArea");
 var reFill1 = document.getElementById("reFillArea1");
 
 function bottomListClick1(){
- if(reFill1.style.visibility = 'visible'){
-  reFill1.style.visibility = "hidden";
+ if(reFill1.visibility = 'visible'){
+  reFill1.visibility = "hidden";
  }else{
-  reFill1.style.visibility = "visible";
+  reFill1.visibility = "visible";
  }
  reFill1 = bodFill;
 }
