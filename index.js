3b038ef35d6529b9d5b5b68e64d0ae7e857bf4f5
EverGen1 <nextgenerationever@gmail.com>
2015-12-19 11:39:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var bodFill = document.getElementById("emptyArea");
+var bodP = document.getElementById("emptyP");
 var botLi2 = document.getElementById("bottomList2");
+function bottomListClick("bodFill", "bodP"){
+ bodP.value = "FAQ";
+ bodP.OnResult( changeOnValue );
+}
+function changeOnValue(){
+ while(bodP.value = 'FAQ'){
+  
