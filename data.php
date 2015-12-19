cf1f92f5ae44074aa5d4421f1f64b557445754c6
EverGen1 <nextgenerationever@gmail.com>
2015-12-19 13:24:46 -0700

    Update data.php

--- data.php
+++ data.php
@@ -1,1 +1,17 @@
 <?php
+if($_POST['submit'] == "SignUp")
+{
+ $varName = $_POST['fullName'];
+ $varEmail = $_POST['Email'];
+ $varPassword = $_POST['Password'];
+ $errorMessage = "";
+if(empty($varName)){
+ $errorMessage .= "<li>Name Required</li>";
+}
+if(empty($varEmail)){
+ $errorMessage .= "<li>Email Required</li>";
+}
+if(empty($varPassword)){
+ $errorMessage .= "<li>Password Required</li>";
+}
+?>
