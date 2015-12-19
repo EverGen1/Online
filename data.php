1fe3d0942566a412349e8f11d932b5a3d27b16a7
EverGen1 <nextgenerationever@gmail.com>
2015-12-19 13:37:36 -0700

    Update data.php

--- data.php
+++ data.php
@@ -1,4 +1,22 @@
 <?php
+$db = mysql_connect("sql3.freemysqlhosting.net", "sql3100432", "3KIBBEuddj");
+if(!$db) die("Error Connecting To Database");
+mysql_select_db("sql3100432", $db);
+$sql = "INSERT INTO movieformdata (Name, Email, Password) VALUES (".
+         PrepSQL($varName) . ", " .
+         PrepSQL($varEmail) . ", " .
+         PrepSQL($varPassword) . ", ";
+  function PrepSQL($value)
+{
+    if(get_magic_quotes_gpc()) 
+    {
+        $value = stripslashes($value);
+    }
+ 
+    $value = "'" . mysql_real_escape_string($value) . "'";
+ 
+    return($value);
+} 
 if($_POST['submit'] == "SignUp")
 {
  $varName = $_POST['fullName'];
@@ -14,4 +32,5 @@ if(empty($varEmail)){
 if(empty($varPassword)){
  $errorMessage .= "<li>Password Required</li>";
 }
+mysql_query($sql);
 ?>
