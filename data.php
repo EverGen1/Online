32346a3f2d6106f09d4a3dcdfc87b722805dbdac
EverGen1 <nextgenerationever@gmail.com>
2015-12-19 13:43:17 -0700

    Update data.php

--- data.php
+++ data.php
@@ -33,4 +33,32 @@ if(empty($varPassword)){
  $errorMessage .= "<li>Password Required</li>";
 }
 mysql_query($sql);
+function Login()
+{
+    if(empty($_POST['username']))
+    {
+        $this->HandleError("UserName is empty!");
+        return false;
+    }
+     
+    if(empty($_POST['password']))
+    {
+        $this->HandleError("Password is empty!");
+        return false;
+    }
+     
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+     
+    if(!$this->CheckLoginInDB($username,$password))
+    {
+        return false;
+    }
+     
+    session_start();
+     
+    $_SESSION[$this->GetLoginSessionVar()] = $username;
+     
+    return true;
+}
 ?>
