38cbbd6b50c87135141c75069058b1c9a9a93d88
EverGen1 <nextgenerationever@gmail.com>
2015-12-22 20:52:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var a = document.getElementById("1");
-var img = document.getElementById("image");
+var a1 = document.getElementById("1");
+
+var img1 = document.getElementById("image");
+
+var div1 = document.getElementById("left");
+
+div1.addEventListener("click", leftChange1);
+
 function leftChange1(){
- if(a.href = 'https://magnr.com'){
-  a.href = "http://moonbit.co.in/?ref=1728b39e8002";
+ 
+ if(a1.href = 'https://magnr.com'){
+  a1.href = "http://moonbit.co.in/?ref=1728b39e8002";
  }else{
-  a.href = "https://magnr.com";
+  a1.href = "https://magnr.com";
  }
  
- if(img.src = 'http://www.evergencompany.co/Images/ads/magnr.png'){
-  img.src = "http://www.evergencompany.co/Images/ads/moon.png";
+ if(img1.src = 'http://www.evergencompany.co/Images/ads/magnr.png'){
+  img1.src = "http://www.evergencompany.co/Images/ads/moon.png";
  }else{
-  img.src = "http://www.evergencompany.co/Images/ads/magnr.png";
+  img1.src = "http://www.evergencompany.co/Images/ads/magnr.png";
  }
+ 
 }
