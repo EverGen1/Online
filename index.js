37f51f650921667e0c2d707216a540a058ccf5e0
EverGen1 <nextgenerationever@gmail.com>
2015-12-22 20:42:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var bodFill = document.getElementById("emptyArea");
-var reFill1 = document.getElementById("reFillArea1");
-
-function bottomListClick1(){
- if(reFill1.visibility = 'visible'){
-  reFill1.visibility = "hidden";
+var a = document.getElementById("1");
+var img = document.getElementById("image");
+function leftChange1(){
+ if(a.href = 'https://magnr.com'){
+  a.href = "http://moonbit.co.in/?ref=1728b39e8002";
  }else{
-  reFill1.visibility = "visible";
+  a.href = "https://magnr.com";
  }
- bodFill.innerHTML = reFill1.innerHTML;
+ 
+ if(img.src = 'http://www.evergencompany.co/Images/ads/magnr.png'){
+  img.src = "http://www.evergencompany.co/Images/ads/moon.png";
+ }else{
+  img.src = "http://www.evergencompany.co/Images/ads/magnr.png";
+ }
 }
-
-
