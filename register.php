0c06f89c014b47a3d8defac7abd940c6c45d264c
EverGen1 <nextgenerationever@gmail.com>
2016-01-17 14:39:22 -0700

    work

--- register.php
+++ register.php
@@ -1,4 +1,20 @@
 <?php
- if(isset($_POST["submit"])){
-  if($_POST["UserName"]==""||$_POST[""])
+ define('DB_HOST', 'sql5.freesqldatabase.com');
+ define('DB_NAME', 'sql5103302');
+ define('DB_USER', 'sql5103302');
+ define('DB_PASSWORD', 'HQlk9PnlKM');
+ 
+ $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+ $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+ 
+ if (mysqli_connect_errno($con)){
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }else{
+  echo “Successfully connected to your database…”;
  }
+ 
+ function SignUp(){
+  $user_name = $_POST['user_name'];
+  $	
+ }
+?>
