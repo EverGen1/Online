eedc0e2de4b0bd00122b051834e97b23e6ae16cd
EverGen1 <nextgenerationever@gmail.com>
2016-01-20 22:23:43 -0700

    Update bitbank.php

--- bitbank.php
+++ bitbank.php
@@ -1,2 +1,33 @@
 <?php
+ $webMaster = 'nextrevoulution@gmail.com';
+ 
+ $full_name = check_input($_POST['full_name'], "Enter Your Name");
+ $email_address = check_input($_POST['email_address'], "Enter Your EMail");
+ $phone_number = check_input($_POST['phone_number'], "EnterYour Phone Number");
+
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
+function show_error($myError)
+{
+?>
+ <html>
+  <body>
+   <p>Please correct the following error:</p>
+   <strong><?php echo $myError; ?></strong>
+   <p>Hit the back button and try again</p>
+  </body>
+ </html>
+<?php
+  exit();
+ }
 ?>
