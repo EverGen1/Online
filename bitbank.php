41ff686145a47fbf4ca248732df996996be45af4
EverGen1 <nextgenerationever@gmail.com>
2016-01-20 21:17:37 -0700

    Update bitbank.php

--- bitbank.php
+++ bitbank.php
@@ -1,7 +1,2 @@
 <?php
- $to = "nextrevoulution@gmail.com";
- $admin = "bitbank@evergenco.us";
- $info = $_REQUEST['full_name', email_address];
- $subject = "Open BitBank Account;
- 
- mail($to, "$subject", "From:" . $admin);
+?>
