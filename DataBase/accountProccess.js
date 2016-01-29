70e51c6035dfdce06618187ae7f78e3a98fbdabe
EverGen1 <nextgenerationever@gmail.com>
2016-01-29 16:41:37 -0700

    add varialbes

---
+++ DataBase/accountProccess.js
@@ -0,0 +1,14 @@
+var bit = document.getElementById("BitBank");
+ var in1 = document.getElementById("in1").innerHTML;
+ var in2 = document.getElementById("in2").innerHTML;
+ var in3 = document.getElementById("in3").innerHTML;
+ var in4 = document.getElementById("in4").innerHTML;
+ var rowB = insertRow(0);
+  var cell1 = insertCell(0);
+  var cell2 = insertCell(1);
+  var cell3 = insertCell(3);
+  var cell4 = insertCell(4);
+  
+function proccess1(){
+ row  
+}
